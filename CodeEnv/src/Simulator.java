66a4155b5d9c6e5e8fd573a1ad3d36fba028cf04
bulbularora <bulbularora123@gmail.com>
2023-06-28 20:13:45 -0600

    added drag coefficient method, changed to 3d.

--- CodeEnv/src/Simulator.java
+++ CodeEnv/src/Simulator.java
@@ -5,48 +5,80 @@ Group Member: Jasmeet Singh, Ahras Ali, Bulbul Arora
 
 public class Simulator {
 
-    //declaring the private classes
-    private double mass;
+    private static final double GRAVITY = 9.81; // m/s^2
+
+    private double[] barrelPose; // [x, y, z, orientation]
     private double radius;
-    private double pos_x;
-    private double pos_y;
-    private double velocity;
-    private double externalForce;
-    private double dragForce;
+    private double mass;
     private double dragCoefficient;
-    //    private double density;
-//    private double area;
-//    private double flowVelocity;
-    private double gravity;
+    private double initialSpeed;
+    private double[] externalForce; // [x, y, z]
 
+    public void setBarrelPose(double x, double y, double z, double angle) {
+        this.barrelPose = new double[]{x, y, z, angle};
+    }
 
-    //making a constructor to ensure the variables are intialized
-
-
-    public Simulator(double mass, double radius, double pos_x, double pos_y, double velocity,
-                     double externalForce, double dragForce, double dragCoefficient, double gravity) {
-        this.mass = mass;
+    public void setRadius(double radius) {
         this.radius = radius;
-        this.pos_x = pos_x;
-        this.pos_y = pos_y;
-        this.velocity = velocity;
-        this.externalForce = externalForce;
-        this.dragForce = dragForce;
-        this.dragCoefficient = dragCoefficient;
-        this.gravity = gravity;
     }
 
-    public Simulator() {
-        mass = 0;
-        this.radius = 0;
-        this.pos_x = 0;
-        this.pos_y = 0;
-        velocity = 0;
-        externalForce = 0;
-        dragForce = 0;
-        dragCoefficient = 0;
-        gravity = 9.8;
+    public void setMass(double mass) {
+        this.mass = mass;
     }
+
+    public void setDragCoefficient(double dragCoefficient) {
+        this.dragCoefficient = dragCoefficient;
+    }
+
+    public void setInitialSpeed(double initialSpeed) {
+        this.initialSpeed = initialSpeed;
+    }
+
+    public void setExternalForce(double x, double y, double z) {
+        this.externalForce = new double[]{x, y, z};
+    }
+
+    public double[] dragForce(double[] flowVelocity) {
+        // flow velocity = [x,y,z]
+        double density = 1.225; // assuming air density at sea level = 1.225 kg / m^3
+        double[] dragForce = new double[3];
+        double dragForceMagnitude;
+        double area = Math.PI * radius * radius; // pi*r^2
+
+        double flowVelocityMagnitude = Math.sqrt(
+                flowVelocity[0] * flowVelocity[0] +
+                        flowVelocity[1] * flowVelocity[1] +
+                        flowVelocity[2] * flowVelocity[2]);  // sqrt(x^2 + y^2 + z^2)
+
+        if(flowVelocityMagnitude !=0) {
+            double[] unit = new double[]{ // unit vector for drag force.
+                    flowVelocity[0] / flowVelocityMagnitude,
+                    flowVelocity[1] / flowVelocityMagnitude,
+                    flowVelocity[2] / flowVelocityMagnitude
+            };
+
+            dragForceMagnitude = -0.5 * this.dragCoefficient * density * area * flowVelocityMagnitude * flowVelocityMagnitude;
+
+            for(int i =0; i<3; i++){
+                dragForce[i] = dragForceMagnitude*unit[i]; // magnitude * x unit component ...
+            }
+
+        }
+
+        return dragForce;
+
+    }
+
+
+}
+
+
+//making a constructor to ensure the variables are intialized
+
+
+
+
+
 
 
 /*
@@ -54,93 +86,93 @@ mass*velocity = external_force + drag_force + mass*gravity ---1
 drag_force = (-0.5)*drag_coefficient*density*area*flow_velocity ---2
  */
 
-    public double getMomentum() {
-        //mass*velocity = external_force + drag_force + mass*gravity ---1
-
-        double p = externalForce + dragForce + mass * gravity;
-        return p;
-    }
-
-    public double calculateDragForce(double density, double area, double flowVelocity) {
-        //drag_force = (-0.5)*drag_coefficient*density*area*flow_velocity ---2
-        this.dragForce = (-0.5) * dragCoefficient * density * area * flowVelocity;
-        return this.dragForce;
-    }
-
-    public double getMass() {
-        return mass;
-    }
-
-    public void setMass(double mass) {
-        this.mass = mass;
-    }
-
-    public double getVelocity() {
-        return velocity;
-    }
-
-    public void setVelocity(double velocity) {
-        this.velocity = velocity;
-    }
-
-    public double getExternalForce() {
-        return externalForce;
-    }
-
-    public void setExternalForce(double externalForce) {
-        this.externalForce = externalForce;
-    }
-
-    public void setDragForce(double dragForce) {
-        this.dragForce = dragForce;
-    }
-
-    public double getDragCoefficient() {
-        return dragCoefficient;
-    }
-
-    public void setDragCoefficient(double dragCoefficient) {
-        this.dragCoefficient = dragCoefficient;
-    }
-
-    public double getGravity() {
-        return gravity;
-    }
-
-    public void setGravity(double gravity) {
-        this.gravity = gravity;
-    }
-
-    public double getRadius() {
-        return radius;
-    }
-
-    public void setRadius(double radius) {
-        this.radius = radius;
-    }
-
-    public double getPos_x() {
-        return pos_x;
-    }
-
-    public void setPos_x(double pos_x) {
-        this.pos_x = pos_x;
-    }
-
-    public double getPos_y() {
-        return pos_y;
-    }
-
-    public void setPos_y(double pos_y) {
-        this.pos_y = pos_y;
-    }
-
-    public double getDragForce() {
-        return dragForce;
-    }
-
-//    public static void main(String[] args){
+//    public double getMomentum() {
+//        //mass*velocity = external_force + drag_force + mass*gravity ---1
 //
+//        double p = externalForce + dragForce + mass * gravity;
+//        return p;
 //    }
-
-}
+//
+//    public double calculateDragForce(double density, double area, double flowVelocity) {
+//        //drag_force = (-0.5)*drag_coefficient*density*area*flow_velocity ---2
+//        this.dragForce = (-0.5) * dragCoefficient * density * area * flowVelocity;
+//        return this.dragForce;
+//    }
+//
+//    public double getMass() {
+//        return mass;
+//    }
+//
+//    public void setMass(double mass) {
+//        this.mass = mass;
+//    }
+//
+//    public double getVelocity() {
+//        return velocity;
+//    }
+//
+//    public void setVelocity(double velocity) {
+//        this.velocity = velocity;
+//    }
+//
+//    public double getExternalForce() {
+//        return externalForce;
+//    }
+//
+//    public void setExternalForce(double externalForce) {
+//        this.externalForce = externalForce;
+//    }
+//
+//    public void setDragForce(double dragForce) {
+//        this.dragForce = dragForce;
+//    }
+//
+//    public double getDragCoefficient() {
+//        return dragCoefficient;
+//    }
+//
+//    public void setDragCoefficient(double dragCoefficient) {
+//        this.dragCoefficient = dragCoefficient;
+//    }
+//
+//    public double getGravity() {
+//        return gravity;
+//    }
+//
+//    public void setGravity(double gravity) {
+//        this.gravity = gravity;
+//    }
+//
+//    public double getRadius() {
+//        return radius;
+//    }
+//
+//    public void setRadius(double radius) {
+//        this.radius = radius;
+//    }
+//
+//    public double getPos_x() {
+//        return pos_x;
+//    }
+//
+//    public void setPos_x(double pos_x) {
+//        this.pos_x = pos_x;
+//    }
+//
+//    public double getPos_y() {
+//        return pos_y;
+//    }
+//
+//    public void setPos_y(double pos_y) {
+//        this.pos_y = pos_y;
+//    }
+//
+//    public double getDragForce() {
+//        return dragForce;
+//    }
+//
+////    public static void main(String[] args){
+////
+////    }
+//
+//}
